493a13cb6b53d260edf65caa273728642ccdda51
“[ReetKubba]” <[rkubba_be20@thapar.edu]>
2022-09-26 22:16:34 +0100

    blog

---
+++ iblog/blog/views.py
@@ -0,0 +1,26 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from blog.models import Post
+from blog.models import Category
+# Create your views here.
+def home(request):
+    #load all post from db(10)
+    posts=Post.objects.all()[:11]
+    #print(posts)
+    cats=Category.objects.all()
+    data={
+        'posts':posts,
+        'cats':cats
+    }
+    return render(request,'home.html',data)
+def post(request,url):
+    post=Post.objects.get(url=url)
+    cats=Category.objects.all()
+    #print(cats)
+    return render(request,'post.html',{'post':post,'cats':cats})
+
+def category(request,url):
+    cat=Category.objects.get(url=url)
+    posts=Post.objects.filter(cat=cat)
+    return render(request,"category.html",{'cat':cat,'posts':posts})
